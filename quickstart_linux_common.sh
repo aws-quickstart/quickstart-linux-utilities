6413634bde919d591fd7a07911ca35919329098b
Tony Vattathil <avattathil@gmail.com>
2017-03-24 09:59:15 -0700

    Added update os function Rename all other function to be consistant

--- quickstart_linux_common.sh
+++ quickstart_linux_common.sh
@@ -6,6 +6,7 @@
 # -Red Hat Enterprise Linux
 # -Centos
 # -Ubuntu
+# -Fedora
 # -Amazon Linux
 # -SUSE
 
@@ -13,113 +14,125 @@
 PROGRAM='QuickStart Linux Common Tools'
 
 # Detects operating system type and return vaule
-# If no varible is pass in function will print to standard out
+# If no varible is passed in function will print to stdout
 # 
-function get_ostype () {
-        local __return=$1
-        DETECTION_STRING="/etc/*-release"
-        if [[ $(ls ${DETECTION_STRING}) ]]; then
-                OS=$(cat /etc/*-release \
-                | grep PRETTY_NAME \
-                | tr -d \" \
-                | awk -F= '{print $2}'\
-                | awk '{print $1,$2}'  \
-                | sed 's/Linux//g' \
-                | sed 's/[^a-zA-Z]//g')
+function qs_get-ostype () {
+    local __return=$1
+    DETECTION_STRING="/etc/*-release"
+    if [[ $(ls ${DETECTION_STRING}) ]]; then
+            OS=$(cat /etc/*-release \
+            | grep PRETTY_NAME \
+            | tr -d \" \
+            | awk -F= '{print $2}'\
+            | awk '{print $1,$2}'  \
+            | sed 's/Linux//g' \
+            | sed 's/[^a-zA-Z]//g')
 
-                if [ $? -eq 0 ] && [ "$__return" ]
-                then
-                        eval $__return="${OS}"
-                        return 0
-                elif [ $OS ]; then
-                        echo $OS
-                        return 0;
-                else
-                     echo "Unknown"
-                fi
+        if [ $? -eq 0 ] && [ "$__return" ];then
+            eval $__return="${OS}"
+            return 0
+        elif [ $OS ]; then
+            echo $OS
+            return 0;
         else
-                if [ "$__return" ]; then
-                        __return="Unknown"
-                        return 1
-                else
-                        echo "Unknown"
-                        return 1;
-                fi
+             echo "Unknown"
         fi
+    else
+        if [ "$__return" ]; then
+                __return="Unknown"
+                return 1
+        else
+                echo "Unknown"
+                return 1;
+        fi
+    fi
 }
 
+# Returns operating system version or return 1
+# If no varible is passed in function will print to stdout
+# 
+function qs_get-osversion () {
+    local __return=$1
+    DETECTION_STRING="/etc/*-release"
+    if [[ $(ls ${DETECTION_STRING})  ]]; then
+        OSLEVEL=$(cat /etc/*-release \
+        | grep VERSION_ID \
+        | tr -d \" \
+        | awk -F= '{print $2}')
 
-function get_osversion () {
-        local __return=$1
-        DETECTION_STRING="/etc/*-release"
-        if [[ $(ls ${DETECTION_STRING})  ]]; then
-                OSLEVEL=$(cat /etc/*-release \
-                | grep VERSION_ID \
-                | tr -d \" \
-                | awk -F= '{print $2}')
-
-                if [ $? -eq 0 ] && [ "$__return" ]
-                then
-                        eval $__return="${OSLEVEL}"
-                        return 0
-                elif [ $OS ]; then
-                        echo $OSLEVEL
-                        return 0;
-                else
-                     echo "Unknown"
-                fi
+        if [ $? -eq 0 ] && [ "$__return" ];then
+            eval $__return="${OSLEVEL}"
+            return 0
+        elif [ $OS ]; then
+            echo $OSLEVEL
+            return 0;
         else
-                if [ "$__return" ]; then
-                        __return="Unknown"
-                        return 1
-                else
-                        echo "Unknown"
-                        return 1;
-                fi
+            echo "Unknown"
         fi
+    else
+        if [ "$__return" ]; then
+            __return="Unknown"
+            return 1
+        else
+            echo "Unknown"
+            return 1;
+        fi
+    fi
 }
 
+# If python is install Returns default python path
+# If no varible is passed in function will print to stdout
+function qs_get-python-path () {
+    local __return=$1
+    # Set PYTHON_EXECUTEABLE to default python version
+    if command -v python > /dev/null 2>&1; then
+       PYTHON_EXECUTEABLE=$(which python)
+    else
+       PYTHON_EXECUTEABLE=$(which python3)
+    fi
 
-function get_python_path () {
-        local __return=$1
-        if command -v python > /dev/null 2>&1; then
-           PYTHON_EXECUTEABLE=$(which python)
-        else
-           PYTHON_EXECUTEABLE=$(which python3)
-        fi
-	if [ $PYTHON_EXECUTEABLE ] && [ "$__return" ]
-                then
-                    eval $__return="${PYTHON_EXECUTEABLE}"
-                    return 0
-                elif [ $PYTHON_EXECUTEABLE ]; then
-                    echo $PYTHON_EXECUTEABLE
-                    return 0;
-                else
-                    echo "Python Not installed"
-		    return 1
-                fi
+    #Return python path or return code (1)
+    if [ $PYTHON_EXECUTEABLE ] && [ "$__return" ]; then
+        eval $__return="${PYTHON_EXECUTEABLE}"
+        return 0
+    elif [ $PYTHON_EXECUTEABLE ]; then
+        echo $PYTHON_EXECUTEABLE
+        return 0;
+    else
+        echo "Python Not installed"
+	    return 1
+    fi
 
 }
 
-# Assigns values to INSTANCE_OSTYPE
-get_ostype INSTANCE_OSTYPE
-get_osversion INSTANCE_OSVERSION
+# Installs pip from pypa
+function  qs_bootstrap_pip () {
+	curl --silent \
+	 --show-error \
+	--retry 5 \
+	https://bootstrap.pypa.io/get-pip.py | sudo qs_get-python-path
+}
 
-def function install_pip () {
+# Updates supported operating systems to latest or exit with code (1)
+# If no varible is passed in function will print to stdout
+function qs_update-os () {
+    # Assigns values to INSTANCE_OSTYPE
+    get_ostype INSTANCE_OSTYPE
+    get_osversion INSTANCE_OSVERSION
 
-if [ "$INSTANCE_OSTYPE" == "AMZN" ]; then
-    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo get_python_path 
-elif [ "$INSTANCE_OSTYPE" == "Ubuntu" ]; then
-    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo get_python_path 
-elif [ "$INSTANCE_OSTYPE" == "RedHat" ]; then
-    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo get_python_path 
-elif [ "$INSTANCE_OSTYPE" == "CentOS" ]; then
-    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo get_python_path 
-elif [ "$INSTANCE_OSTYPE" == "fedora" ]; then
-    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo get_python_path 
-elif [ "$INSTANCE_OSTYPE" == "SUSE" ]; then
-    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo get_python_path 
-else
-    echo "[ERROR] ${INSTANCE_OSTYPE) not Supported"
-    exit 1
-
+    if [ "$INSTANCE_OSTYPE" == "AMZN" ]; then
+    	yum update -y
+    elif [ "$INSTANCE_OSTYPE" == "Ubuntu" ]; then
+    	apt-get update -y
+    elif [ "$INSTANCE_OSTYPE" == "RedHat" ]; then
+    	yum update -y
+    elif [ "$INSTANCE_OSTYPE" == "CentOS" ]; then
+    	yum update -y
+    elif [ "$INSTANCE_OSTYPE" == "fedora" ]; then
+    	dnf update -y	
+    elif [ "$INSTANCE_OSTYPE" == "SUSE" ]; then
+    	zypper refresh && zypper update
+    else
+        exit 1
+    fi
+}
